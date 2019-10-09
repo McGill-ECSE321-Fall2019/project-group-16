ee09a5c5e1da5b5c6c7391285649c76fee6ba65c
Mustafain Ali <mustafain.khan@mail.mcgill.ca>
2019-10-09 16:41:29 -0400

    initial commit of the backend application

---
+++ TutoringSystem-Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/TutoringSystemApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.tutoringsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoringSystemApplication.class, args);
+	}
+
+}
