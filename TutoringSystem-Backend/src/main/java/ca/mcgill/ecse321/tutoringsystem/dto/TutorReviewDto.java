8c7591ea55c107d6f858935ace6a84fcfeb110ec
jerryled <1196860585@qq.com>
2019-10-31 17:23:50 -0400

    tutor review

---
+++ TutoringSystem-Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dto/TutorReviewDto.java
@@ -0,0 +1,38 @@
+package ca.mcgill.ecse321.tutoringsystem.dto;
+
+import ca.mcgill.ecse321.tutoringsystem.model.Student;
+import ca.mcgill.ecse321.tutoringsystem.model.Tutor;
+
+public class TutorReviewDto {
+	private int id;
+	private String review;
+	private Tutor reviewee;
+	private int rating;
+	private  Student author;
+	
+	private String authorName;
+	private String revieweeName;
+	
+	
+
+	public TutorReviewDto(int id, String review, Tutor reviewee, int rating, Student author) {
+		this.id = id;
+		this.review = review;
+		this.authorName = author.getName();
+		this.revieweeName = reviewee.getName();	
+	}
+	
+	public void setReview(String review) {
+		this.review = review;
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getId() {
+		return id;
+	}
+
+}
