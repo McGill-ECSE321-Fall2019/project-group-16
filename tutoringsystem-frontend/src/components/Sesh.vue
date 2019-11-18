bb2485bd3d0992e7c62fb58efa50bb89a148df3e
talha-riaz <talha.riaz@mail.mcgill.ca>
2019-11-17 21:56:26 -0500

    Commit View Sessions

---
+++ tutoringsystem-frontend/src/components/Sesh.vue
@@ -0,0 +1,62 @@
+<template>
+<span class="container">
+   
+<div id="table">
+  <h6>All Current Sessions</h6>
+  <table class="table table-bordered">
+   <tr>
+      <th>Session ID</th>
+      <th>Student</th>
+      <th>Tutor</th>
+      <th>Room</th>
+      <th>Course</th>
+      <th>Date</th>
+      <th>Start Time</th>
+      <th>End Time</th>
+      <th>Group Session</th>
+      <th>Confirmed</th>
+    </tr>
+   <tr v-for="session in sessions" >
+      <td>{{ session.id }}</td>
+      <td>{{ session.student }}</td>
+      <td>{{ session.tutor }}</td>
+      <td>{{ session.room }}</td>
+      <td>{{ session.course }}</td>
+      <td>{{ session.date }}</td>
+      <td>{{ session.startTime }}</td>
+      <td>{{ session.endTime }}</td>
+      <td>{{ session.isGroupSession }}</td>
+      <td>{{ session.isConfirmed }}</td>
+  </tr>
+<!-- ... -->
+</table>
+</div>
+
+<h6>Make sure to update the session list!</h6>
+
+<b-button @click="getSessions()" variant="outline-info">Update Sessions</b-button>
+ <br><br>
+
+</span>
+</template>
+
+<script src="./getSessions.js">
+</script>
+
+
+<style scoped>
+
+#table{
+  margin-left: 20px;
+  margin-right: 20px;
+  position: center;
+}
+#selectUni {
+  margin-right: 50px; 
+  margin-left: 50px;
+  padding: 10px;
+  max-width: 100%;
+  margin-bottom: 10px;
+}
+
+</style>
---
+++ tutoringsystem-frontend/src/components/getSessions.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+var config = require('../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+
+function SessionDto(name) {
+  this.name = name
+}
+
+export default{
+  name: 'sessions',
+  data () {
+    return {
+      sessions: [],
+      newSession: '',
+      selectedSessions: '',
+      errorSession: '',
+    }
+  },
+  methods : {
+      getSessions: function () {
+              // Initializing people from backend
+                AXIOS.get(`/currentsesh`)
+                .then(response => {
+                  // JSON responses are automatically parsed.
+                  this.sessions = response.data
+                  this.errorSession =''
+                })
+                .catch(e => {
+                  this.errorSession = e.response.data.message;
+                });
+        },
+        
+}
+
+
+
+
+
+
+}
