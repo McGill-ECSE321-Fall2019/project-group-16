8288b1ca44e0232464ef5388b0da19adb6041f28
Mustafain Ali Khan <mustafain.khan@mail.mcgill.ca>
2019-12-01 23:25:24 -0500

    completed booking session

--- TutoringSystem-Android/app/src/main/java/ca/mcgill/ecse321/tutoringsystem/MainActivity.java
+++ TutoringSystem-Android/app/src/main/java/ca/mcgill/ecse321/tutoringsystem/MainActivity.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.tutoringsystem;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,10 +12,9 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private String error;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        // initialize error message text view
-        refreshErrorMessage();
     }
 
     @Override
@@ -55,16 +53,9 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    private void refreshErrorMessage() {
-        // set the error message
-        TextView tvError = (TextView) findViewById(R.id.error);
-        tvError.setText(error);
 
-        if (error == null || error.length() == 0) {
-            tvError.setVisibility(View.GONE);
-        } else {
-            tvError.setVisibility(View.VISIBLE);
-        }
+    public void gotoSession(View view){
+        Intent intent = new Intent(this, BookSessionActivity.class);
+        startActivity(intent);
     }
-
 }
