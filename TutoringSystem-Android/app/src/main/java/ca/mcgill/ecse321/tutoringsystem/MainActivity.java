afeadb5d31ae2bec04eabfea904c8e5108e8cb68
zhengyucui <cuizhengyu@hotmail.com>
2019-12-03 11:01:21 -0500

    finished register screen

--- TutoringSystem-Android/app/src/main/java/ca/mcgill/ecse321/tutoringsystem/MainActivity.java
+++ TutoringSystem-Android/app/src/main/java/ca/mcgill/ecse321/tutoringsystem/MainActivity.java
@@ -1,17 +1,25 @@
 package ca.mcgill.ecse321.tutoringsystem;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import cz.msebera.android.httpclient.Header;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private String error;
@@ -65,6 +73,54 @@ public class MainActivity extends AppCompatActivity {
         } else {
             tvError.setVisibility(View.VISIBLE);
         }
+    }
+
+    public void register(View v) {
+        error = "";
+        final TextView nameTV = (TextView) findViewById(R.id.newstudent_name);
+        final TextView usernameTV = (TextView)  findViewById(R.id.newstudent_username);
+        final TextView passwordTV = (TextView) findViewById(R.id.newstudent_password);
+        final TextView confirmedPWTV = (TextView) findViewById(R.id.newstudent_confirmpassword);
+
+        String name = nameTV.getText().toString();
+        String username = usernameTV.getText().toString();
+        String password = passwordTV.getText().toString();
+        String confirmedPW = confirmedPWTV.getText().toString();
+
+        if(username.trim().length() == 0){
+            error +="Username can't be empty.\n";
+        }
+        if(password.trim().length() == 0){
+            error +="Password can't be empty.\n";
+        }
+        if(name.trim().length() == 0){
+            error +="Name can't be empty.\n";
+        }
+        if(password.equals(confirmedPW)) {
+            error +="Password must match.\n";
+        }
+        if(error.length() != 0){
+            refreshErrorMessage();
+            return;
+        }
+        HttpUtils.post("persons/" + usernameTV.getText().toString() + "/" + passwordTV.getText().toString()
+                + "/" + nameTV.getText().toString(), new RequestParams(), new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                refreshErrorMessage();
+                startActivity(new Intent(MainActivity.this, DummyHomePage.class));
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
     }
 
 }
